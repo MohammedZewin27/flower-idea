cb7fc67e88b1c6ad9f526594b674fe445d05bc38
admin <mohammedzewin01@gmail.com>
2025-02-22 06:25:40 +0300

    1

---
+++ lib/feature/client_one/presentation/pages/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:idea/core/resources/style_manager.dart';
+import 'package:idea/core/widgets/custom_background_screen.dart';
+
+import '../../../../core/resources/color_manager.dart';
+import '../../../../core/widgets/custom_background.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomBackground(
+        assetsImagePng: 'images/image_1.jpg',
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 150,
+              ),
+              Text(
+                'حبي واعتزاز الي زوجتي الحبية وروح قلبي وحياتي كلها',
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.center,
+                style:
+                    getSemiBoldStyle(color: ColorManager.white, fontSize: 45),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'زوجك المخلص',
+                style:
+                    getSemiBoldStyle(color: ColorManager.white, fontSize: 20),
+              ),
+              Align(
+                  alignment: AlignmentDirectional.centerEnd,
+                  child: Image.asset("assets/images/flower1.png"))
+            ],
+          ),
+        ));
+  }
+}
