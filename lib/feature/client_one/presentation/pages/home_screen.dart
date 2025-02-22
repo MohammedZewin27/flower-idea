d9563b23162841ff7f60f15864705361f04adae7
admin <mohammedzewin01@gmail.com>
2025-02-22 08:18:37 +0300

    sss

--- lib/feature/client_one/presentation/pages/home_screen.dart
+++ lib/feature/client_one/presentation/pages/home_screen.dart
@@ -8,44 +8,43 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-          Image.asset(
-            'images/image_1.jpg',
-          ),
-          Image.asset("assets/images/flower1.png"),
-          SizedBox(
-                height: 150,
-              ),
-              Text(
-                'حبي  واعتزاز الي زوجتي الحبية وروح قلبي وحياتي كلها',
-                textDirection: TextDirection.rtl,
-                textAlign: TextAlign.center,
-                style:
-                    getSemiBoldStyle(color: ColorManager.white, fontSize: 45),
-              ),
-              Text(
-                'حبي  واعتزاز الي زوجتي الحبية وروح قلبي وحياتي كلها',
-                textDirection: TextDirection.rtl,
-                textAlign: TextAlign.center,
-                style:
-                    getSemiBoldStyle(color: ColorManager.white, fontSize: 45),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'زوجك المخلص',
-                style:
-                    getSemiBoldStyle(color: ColorManager.white, fontSize: 20),
-              ),
-              Align(
-                  alignment: AlignmentDirectional.centerEnd,
-                  child: Image.asset("assets/images/flower1.png"))
-            ],
-          ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/image_1.jpg',
+            ),
+            Image.asset("assets/images/flower1.png"),
+            SizedBox(
+              height: 150,
+            ),
+            Text(
+              'حبي  واعتزاز الي زوجتي الحبية وروح قلبي وحياتي كلها',
+              textDirection: TextDirection.rtl,
+              textAlign: TextAlign.center,
+              style: getSemiBoldStyle(color: ColorManager.orange, fontSize: 45),
+            ),
+            Text(
+              'حبي  واعتزاز الي زوجتي الحبية وروح قلبي وحياتي كلها',
+              textDirection: TextDirection.rtl,
+              textAlign: TextAlign.center,
+              style: getSemiBoldStyle(color: ColorManager.orange, fontSize: 45),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'زوجك المخلص',
+              style: getSemiBoldStyle(color: ColorManager.white, fontSize: 20),
+            ),
+            Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Image.asset("assets/images/flower1.png"))
+          ],
+        ),
+      ),
     );
   }
 }
